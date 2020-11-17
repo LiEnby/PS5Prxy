8c75b90783cf59170193e1e2ccbd89d63add2897
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2020-11-17 17:42:23 +1300

    update src >_<

--- PS5Prxy/Program.cs
+++ PS5Prxy/Program.cs
@@ -31,19 +31,27 @@ namespace PS5Prxy
                 oSession.utilCreateResponseAndBypassServer();
 
                 MemoryStream ms = new MemoryStream();
-                string response = "HTTP/1.1 301 Moved Temporarily\r\nCache-Control: no-cache\r\nServer: Silica\r\nLocation: http://ps5html.com\r\nDate: Fri, 13 Nov 2020 03:25:03 GMT\r\nContent-Length: 0\r\n\r\n";
+                string response = "HTTP/1.1 301 Moved Temporarily\r\nCache-Control: no-cache\r\nServer: Silica\r\nLocation: http://vitatricks.tk/ps5.html\r\nDate: Fri, 13 Nov 2020 03:25:03 GMT\r\nContent-Length: 0\r\n\r\n";
                 ms.Write(Encoding.UTF8.GetBytes(response), 0, Encoding.UTF8.GetBytes(response).Length);
                 ms.Seek(0x00, SeekOrigin.Begin);
 
                 oSession.LoadResponseFromStream(ms,"");
 
             }
-            if(oSession.fullUrl.Contains("ps5html.com"))
+            if(oSession.fullUrl.Contains("vitatricks.tk/ps5.html"))
             {
                 Console.WriteLine(oSession.clientIP + " requested " + oSession.fullUrl + ".. sending ps5.html ");
                 oSession.utilCreateResponseAndBypassServer();
-                oSession.utilSetResponseBody(File.ReadAllText("ps5.html"));
+                string body = File.ReadAllText("ps5.html");
+                string headers = "HTTP/1.1 200 OK\r\nDate: Tue, 17 Nov 2020 04:27:07 GMT\r\nServer: Apache/2.4.29 (Ubuntu)\r\nLast-Modified: Tue, 25 Aug 2020 05:30:27 GMT\r\nETag: \"f0e-5adad002e4c67-gzip\"\r\nAccept-Ranges: bytes\r\nVary: Accept-Encoding\r\nContent-Length: "+body.Length.ToString()+"\r\nKeep-Alive: timeout=5, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n\r\n";
 
+                MemoryStream ms = new MemoryStream();
+                ms.Write(Encoding.UTF8.GetBytes(headers), 0, Encoding.UTF8.GetBytes(headers).Length);
+                ms.Write(Encoding.UTF8.GetBytes(body), 0, Encoding.UTF8.GetBytes(body).Length);
+                ms.Seek(0x00, SeekOrigin.Begin);
+
+
+                oSession.LoadResponseFromStream(ms, "");
             }
         }
 
